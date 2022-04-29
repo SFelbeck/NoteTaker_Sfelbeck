333a620d7741d81b90452c4f7ed6d4babb4bcea3
SFelbeck <stefanfelbeck@gmail.com>
2022-04-28 21:46:07 -0700

    submittable(?)

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const router = express.Router();
+// const vars to connect uuidv4, express.Router, and fs
+const { v4: uuidv4 } = require('uuid');
+const router = require('express').Router()
 const fs = require('fs');
-const {uuid} = require('uuidv4');
 
+//router.get to parse the db.json
 router.get('/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('db/db.json', "utf-8"))
+    const notes = JSON.parse(fs.readFileSync('db/db.json'))
     res.json(notes)
 });
 
+//router.post path
 router.post('/notes', (req, res) => {
+// structures any new notes for the db.json
     const newNotes = {
-        title: req.title,
-        text: req.text,
+        title: req.body.title,
+        text: req.body.text,
         id: uuidv4()
     }
-    notes.push(newNotes);
-})
-
-module.exports = router;
+    //parses all notes and pushes new notes to the array
+    const notes = JSON.parse(fs.readFileSync("db/db.json"));
+    notes.push(newNotes)
+    //stringifies all notes in db.json and returns them
+    fs.writeFileSync('db/db.json', JSON.stringify(notes))
+    res.json(notes);
+});
+// exports the module
+    module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+//sets up paths to express, path, and routes.js
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/routes.js');
-
-const PORT = process.env.port || 4200;
-
+const api = require('./routes/routes.js');
+//sets the port to 4000
+const PORT = process.env.PORT || 4000;
+//sets an app const to express
 const app = express();
-
+//sets up express and then connects the api path
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", routes);
+app.use("/api", api);
 
 app.use(express.static('public'));
-
+//app.get route to the notes html
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+//app.get route to the index html
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+//listens to the port
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
